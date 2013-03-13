8ad7d21f05aafff7ec423abb6a39ed1a477b808b
Ingo Albrecht <prom@berlin.ccc.de>
2013-03-13 05:21:26 +0100

    Added some commandline params

--- src/main/java/com/hoccer/talk/server/TalkServerMain.java
+++ src/main/java/com/hoccer/talk/server/TalkServerMain.java
@@ -2,30 +2,53 @@ package com.hoccer.talk.server;
 
 import java.net.InetSocketAddress;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.websocket.WebSocketHandler;
 
 public class TalkServerMain {
 
+    @Parameter(names={"-l", "-listen"},
+               description = "Address/host to listen on")
+    String listen = "0.0.0.0";
+
+    @Parameter(names={"-p", "-port"},
+               description = "Port to listen on")
+    int port = 8080;
+
+    private void run() {
+        // create the talk server
+        TalkServer ts = new TalkServer();
+
+        // create jetty instance
+        Server s = new Server(new InetSocketAddress(listen, port));
+
+        // default handler for non-talk http requests
+        DefaultHandler fallbackHandler = new DefaultHandler();
+        fallbackHandler.setServeIcon(false);
+
+        // handler for talk websocket connections
+        WebSocketHandler clientHandler = new TalkRpcConnectionHandler(ts);
+        clientHandler.setHandler(fallbackHandler);
+
+        // set root handler of the server
+        s.setHandler(clientHandler);
+
+        // run and stop when interrupted
+        try {
+            s.start();
+            s.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 	public static void main(String[] args) {
-		Server s = new Server(new InetSocketAddress("0.0.0.0", 8080));
-		try {
-			TalkServer ts = new TalkServer();
-			
-			DefaultHandler clientDefHandler = new DefaultHandler();
-			clientDefHandler.setServeIcon(false);
-			
-			WebSocketHandler clientHandler = new TalkRpcConnectionHandler(ts);
-			clientHandler.setHandler(clientDefHandler);
-			
-			s.setHandler(clientHandler);
-			
-			s.start();
-			s.join();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        TalkServerMain main = new TalkServerMain();
+        JCommander commander = new JCommander(main, args);
+        main.run();
 	}
 
 }
