e3158943644701dfeacb1082c68a23b3741d360c
Ingo Albrecht <prom@berlin.ccc.de>
2013-03-23 00:16:54 +0100

    Various client library changes
    * Renamed interfaces to prevent confusion
    * Implemented autodisconnect

---
+++ src/main/java/com/hoccer/talk/server/delivery/DeliveryAgent.java
@@ -0,0 +1,11 @@
+package com.hoccer.talk.server.delivery;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: prom
+ * Date: 3/20/13
+ * Time: 5:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeliveryAgent {
+}
