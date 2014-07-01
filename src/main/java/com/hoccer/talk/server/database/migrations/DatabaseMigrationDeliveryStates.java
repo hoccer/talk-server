60124e18674620fef0fba374d1d8133f018acfa9
Andreas Marr <andreasm@artcom.de>
2014-07-01 14:05:57 +0200

    realized delivery states migration.

--- src/main/java/com/hoccer/talk/server/database/migrations/DatabaseMigrationDeliveryStates.java
+++ src/main/java/com/hoccer/talk/server/database/migrations/DatabaseMigrationDeliveryStates.java
@@ -1,13 +1,23 @@
 package com.hoccer.talk.server.database.migrations;
 
+import com.hoccer.talk.model.TalkDelivery;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
 public class DatabaseMigrationDeliveryStates extends BaseDatabaseMigration implements IDatabaseMigration {
 
     // DO not change!
     private final static String MIGRATION_NAME= "2014_06_30_delivery_states";
 
+    private final static Logger LOG = Logger.getLogger(DatabaseMigrationDeliveryStates.class);
+
     @Override
     public void up() {
-        // TODO: implement me (mDatabase is availble!)
+        migrateFromDeliveriesFromStateToState(TalkDelivery.STATE_DELIVERED_OLD, TalkDelivery.STATE_DELIVERED_PRIVATE);
+        migrateFromDeliveriesFromStateToState(TalkDelivery.STATE_CONFIRMED_OLD, TalkDelivery.STATE_DELIVERED_PRIVATE_ACKNOWLEDGED);
+        migrateFromDeliveriesFromStateToState(TalkDelivery.STATE_ABORTED_OLD, TalkDelivery.STATE_ABORTED_ACKNOWLEDGED);
+        migrateFromDeliveriesFromStateToState(TalkDelivery.STATE_FAILED_OLD, TalkDelivery.STATE_FAILED_ACKNOWLEDGED);
     }
 
     @Override
@@ -20,4 +30,12 @@ public class DatabaseMigrationDeliveryStates extends BaseDatabaseMigration imple
         return MIGRATION_NAME;
     }
 
+    private void migrateFromDeliveriesFromStateToState(String startState, String targetState) {
+        final List<TalkDelivery> deliveries = mDatabase.findDeliveriesInState(startState);
+        for (TalkDelivery delivery : deliveries) {
+            delivery.setState(targetState);
+            mDatabase.saveDelivery(delivery);
+        }
+        LOG.info("migrated state for " + deliveries.size() + " deliveries from: '" + startState + "' to '" + targetState + "'");
+    }
 }
